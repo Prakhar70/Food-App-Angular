1ae84c92dd5a18461c329bee653f6225b8ed0947
prakhar <pagarwal@meruig.com>
2021-09-23 23:58:11 +0530

    wrap up & next changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] =[
+    new Recipe('A Test Recipe','This is simply a test','https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800'),
+    new Recipe('A Test Recipe','This is simply a test','https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800')
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
